00e2c13a08ca7745b97c405158a4a352675e29d9
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-06-12 11:49:06 +0200

    Add font awesome (cdn), props on PageHeader (get data from App) and populated "li" on navbar via them

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,33 @@ export default {
   },
   data() {
     return {
-      store
+      store,
+      menuHeader: [
+        {
+          name: "home",
+          url: "#",
+          active: false,
+          dropdown: false
+        },
+        {
+          name: "landing",
+          url: "#",
+          active: true,
+          dropdown: true
+        },
+        {
+          name: "pages",
+          url: "#",
+          active: false,
+          dropdown: true
+        },
+        {
+          name: "docs",
+          url: "#",
+          active: false,
+          dropdown: true
+        },
+      ]
     }
   },
   methods: {
@@ -25,7 +51,7 @@ export default {
 </script>
 
 <template>
-  <PageHeader />
+  <PageHeader :vociMenuHeader="menuHeader" />
   <PageMain />
   <PageFooter />
 </template>
@@ -34,4 +60,5 @@ export default {
 @use './styles/general.scss' as *;
 @use './styles/partials/variables.scss' as *;
 @use './styles/partials/mixins.scss' as *;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 </style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,16 +1,112 @@
 <script>
 export default {
     name: "PageHeader",
+    props: {
+        vociMenuHeader: Array
+    }
 }
 </script>
 
 <template>
     <header>
+        <nav>
+            <div class="logo">
+                <a href="#">
+                    <img src="./../assets/img/logo-dark.png" alt="logo-dark">
+                </a>
+            </div>
 
+            <ul>
+                <li v-for="voceMenu in vociMenuHeader">
+                    <a :href="voceMenu.url" :class="(voceMenu.active) ? 'navActive' : ''">{{ voceMenu.name.toUpperCase()
+                    }}</a>
+                    <span v-if="voceMenu.dropdown" :class="(voceMenu.active) ? 'navActive' : ''"><i
+                            class="fa-solid fa-chevron-down"></i></span>
+                </li>
+            </ul>
+
+            <div class="icon">
+                <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                <a href="#"><i class="fa-brands fa-github"></i></a>
+                <a href="#"><i class="fa-brands fa-stack-overflow"></i></a>
+                <a href="#"><i class="fa-regular fa-user"></i></a>
+            </div>
+        </nav>
     </header>
 </template>
 
 <style scoped lang="scss">
 @use '../styles/partials/variables.scss' as *;
 @use '../styles/partials/mixins.scss' as *;
+
+header {
+    background-color: #f8f9fc;
+    width: 100%;
+    padding: 10px 0;
+
+    nav {
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .logo {
+            width: 130px;
+
+            img {
+                max-width: 100%;
+            }
+        }
+
+        ul {
+            display: flex;
+            gap: 25px;
+            font-weight: 500;
+
+            .navActive {
+                color: #355ad5;
+            }
+
+            span {
+                font-size: 10px;
+                margin-left: 5px;
+                position: relative;
+                bottom: 2px;
+            }
+        }
+
+        .icon {
+            display: flex;
+            align-items: center;
+            gap: 8px;
+
+            .fa-magnifying-glass {
+                color: #8e9bad;
+                margin-right: 5px;
+            }
+
+            .fa-brands,
+            .fa-regular {
+                color: #3a5ed5;
+                background-color: #e4e9f8;
+                padding: 10px;
+                border-radius: 5px;
+                border: 1px solid #d8e0f6;
+            }
+
+            .fa-user {
+                font-size: 12px;
+                padding: 12px;
+            }
+
+            .fa-brands:hover,
+            .fa-regular:hover {
+                background-color: #2f55d4;
+                color: white;
+            }
+        }
+
+    }
+}
 </style>
